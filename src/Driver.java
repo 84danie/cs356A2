53d53cbece32792b2f68d5eb187ba1177a99a044
84danie <danielleholzberger@gmail.com>
2017-10-29 10:38:17 -0700

    Implemented User class.

--- src/Driver.java
+++ src/Driver.java
@@ -6,11 +6,14 @@ public class Driver {
 	public static void main(String[] args) {
 		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	            public void run() {
-	            	AdminControlPanel panel = null;
-	        		panel.getInstance();
-	                
+	        		AdminControlPanel.getInstance();  
 	            }
 	        });
+		User u1 = new User("Joe");
+		User u2 = new User("Joe2");
+		u1.addObserver(u2);
+		System.out.println(u1.getFollowers());
+		System.out.println(u2.getFollowings());
 		
 
 	}
--- src/User.java
+++ src/User.java
@@ -1,18 +1,65 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.UUID;
 
+/**
+ * User class. Users can follow and be followed by other users.
+ *
+ */
 public class User extends Observable implements Observer{
+	private String id;
+	private String visibleID;
+	private List<User> followers;
+	private List<User> followings;
+	private List<String> newsFeed;
 
-	public User() {
-		// TODO Auto-generated constructor stub
+	public User(String visibleID) {
+		this.id = UUID.randomUUID().toString();
+		this.visibleID = visibleID;
+		this.followers = new ArrayList<User>();
+		this.followings = new ArrayList<User>();
+	}
+	public void addObserver(Observer o){
+		if(o instanceof User){
+			super.addObserver(o);
+			followers.add((User)o);
+			((User)o).followings.add(this);
+		}
+		else
+			System.out.println("NOT A USER!");
+	}
+	public void postMessage(String message){
+		newsFeed.add(message);
+		setChanged();
+		notifyObservers(message);
 	}
 
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		if(arg instanceof String){
+			newsFeed.add((String)arg);
+		}
 	}
-	
-
+	public String getId() {
+		return id;
+	}
+	public String getVisibleID() {
+		return visibleID;
+	}
+	public List<User> getFollowers() {
+		return followers;
+	}
+	@Override
+	public String toString() {
+		return visibleID;
+	}
+	public List<User> getFollowings() {
+		return followings;
+	}
+	public List<String> getNewsFeed() {
+		return newsFeed;
+	}
 }
