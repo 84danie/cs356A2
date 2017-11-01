8d4e71d19a6417256654f8a45f66d7032d3c13b8
84danie <danielleholzberger@gmail.com>
2017-10-31 20:08:33 -0700

    Added Composite class

--- src/Driver.java
+++ src/Driver.java
@@ -4,24 +4,39 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	        		AdminControlPanel.getInstance();  
-	            }
-	        });
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				AdminControlPanel.getInstance();  
+			}
+		});
 		User u1 = new User("Joe");
 		User u2 = new User("Joe2");
 		u1.addObserver(u2);
 		System.out.println(u1.getFollowers());
 		System.out.println(u2.getFollowings());
-		
+
 		User u3 = new User("Bob");
 		u1.addObserver(u3);
 		System.out.println(u1.getFollowers());
 		System.out.println(u2.getFollowings());
 		System.out.println(u3.getFollowings());
-		
 
+		System.out.println("Test UserGroup:");
+
+		CompositeUserGroup Root = new CompositeUserGroup("Root");
+		CompositeUserGroup Group1 = new CompositeUserGroup("Group 1");
+		CompositeUserGroup Group2 = new CompositeUserGroup("Group 2");
+
+		Group1.add(u2);
+		Root.add(u1);
+		Group2.add(u3);
+		Root.add(Group1);
+		Group1.add(Group2);
+		Root.add(u1);
+
+		Root.add(Group2);
+
+		System.out.println(Root.print(" "));
 	}
 
 }
---
+++ src/UserGroup.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface UserGroup {
+	/**
+	 * Printing method
+	 * @param space
+	 * @return
+	 */
+	public String print(String space);
+
+}
